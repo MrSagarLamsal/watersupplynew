7dd763beca8ca5a67039299d93e33a4adfafc959
BishalRaj <bisal1shakya@gmail.com>
2019-07-12 17:17:52 +0545

    recycler finish

---
+++ app/src/main/java/com/example/watersupply_kathmandu/API/OrderApi.java
@@ -0,0 +1,15 @@
+package com.example.watersupply_kathmandu.API;
+
+import com.example.watersupply_kathmandu.Models.OrderModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface OrderApi {
+    @GET("/showorderdetails")
+    Call<List<OrderModel>> getOrderDetails();
+}
